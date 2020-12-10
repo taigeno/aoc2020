1ff6d9a549306bb4cb6e82b68a07e412e8dd5813
Taigen O <taigen@gmail.com>
2020-12-09 21:52:18 -0500

    Day 2 using combinations

--- Day1/day1.py
+++ Day1/day1.py
@@ -1,38 +1,21 @@
 
 from math import prod
+from itertools import combinations
 
 with open('input.txt', 'r') as inputFile:
     lines = [line.strip() for line in inputFile]
 
-def part_one():
-    opp = set()
-    for line in lines:
-        num = int(line)
-        num2 = 2020 - num
 
-        if num in opp:
-            print(num * num2)
-            return
-        else:
-            opp.add(num2)
+def get_sum(num):
+    for comb in combinations(lines, num):
+        if sum(comb) == 2020:
+            print(prod(comb))    
+
+def part_one():
+    get_sum(2)
 
 def part_two():
-    singles = {}
-    pairs = {}
-    for line in lines:
-        num = int(line)
-        
-        for pair in pairs:
-            if 2020 == int(pair) + num:
-                print(prod(pairs[pair]) * num)
-                return
-
-        for single in singles:
-            s = singles[single] + num 
-            if s < 2020:
-                pairs[s] = (singles[single], num)        
-
-        singles[num] = num
+    get_sum(3)
 
 part_one()
 part_two()
